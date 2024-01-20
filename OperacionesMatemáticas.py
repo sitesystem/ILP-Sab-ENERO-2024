ebd54b863221b926c22b1149cfbd6fcc21ac1169
sitesystem <l_aloaxa05@hotmail.com>
2024-01-20 13:04:11 -0600

    Avance Ejemplo 2

---
+++ OperacionesMatemáticas.py
@@ -0,0 +1,18 @@
+# Operaciones Matemáticas
+
+# ENTRADA DE DATOS 
+# Declarar, crear o instancias variables o constantes
+número_1 = 10
+número_2 = 2.5
+
+# PROCESOS (Cálculos y operaciones matemáticas y lógicas)
+suma = número_1 + número_2
+resta = número_1 - número_2
+multiplicación = número_1 * número_2
+división = número_1 / número_2
+
+# SALIDA DE DATOS
+print("La suma es =", suma)
+print('La resta es =', resta)
+print(multiplicación)
+print(división)
